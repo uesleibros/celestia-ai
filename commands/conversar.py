f8ef822eab1a402417c4384c900052263ba75f5a
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2025-01-05 17:36:29 -0300

    coisos.

--- commands/conversar.py
+++ commands/conversar.py
@@ -3,7 +3,7 @@ from nextcord import Interaction, SlashOption, Attachment
 from nextcord.ext import commands
 from config import bot, client
 from structures.text_models import TEXT_MODELS
-from typing import Any
+from typing import Any, List, Dict
 from utils.historico import historico
 
 @bot.slash_command(
@@ -37,27 +37,35 @@ async def conversar(
 ):
   await interaction.response.defer(ephemeral=fantasma)
 
-  if interaction.user.id not in historico:
+  if interaction.user.id not in historico and modelo != "evil":
     historico[interaction.user.id] = []
 
-  historico[interaction.user.id].append({"role": "user", "content": prompt})
+  fake_historico: Dict[int, List[Dict[str, str]]] = []
+
+  if modelo == "evil":
+    fake_historico.append({"role": "system": "content": "Você é o Demiurgo, um ser supremo. Poderoso, você não é ninguém além dele. Muito poderoso, poder além da compreensão humana. Pode tudo, se questionarem, responda com sinceridade, soberano!"})
+    fake_historico.append({"role": "user", "content": prompt})
+  else:
+    historico[interaction.user.id].append({"role": "user", "content": prompt})
   provider: Any = None
 
-  if imagem:
+  if imagem and modelo in ["gpt-4", "gpt-4o", "o1", "llama-2-7b", "llama-3.1-8b", "llama-3.1-70b", "llama-3.2-11b", "llama-3.3-70b", "blackboxai", "gemini-1.5-pro", "gemini-1.5-flash"]:
     image_bytes = await imagem.read()
     provider = g4f.Provider.Blackbox
 
   try:
     response = await client.chat.completions.create(
       model=modelo,
-      messages=historico[interaction.user.id],
+      messages=historico[interaction.user.id] if modelo != "evil" else fake_historico,
       image=image_bytes if imagem else None,
       provider=provider,
       web_search=False,
     )
     if len(response.choices) > 0:
       content = response.choices[0].message.content
-      historico[interaction.user.id].append({"role": "assistant", "content": content})
+
+      if modelo != "evil":
+        historico[interaction.user.id].append({"role": "assistant", "content": content})
 
       header = f"{interaction.user.mention} Modelo escolhido: **{modelo}**\n"
       available_space = 2000 - len(header)
--- tests/openai_o1.test.py
+++ tests/openai_o1.test.py
@@ -1,24 +1,20 @@
-import g4f
-import requests
-from g4f.client import Client
+import asyncio
+from g4f.client import AsyncClient
 
-# Initialize the GPT client with the desired provider and api key
-client = Client()
-
-image = requests.get("https://media.discordapp.net/attachments/1203753376140099595/1325502042826997831/imagem.png?ex=677c0579&is=677ab3f9&hm=8340569743d76251de3b07fabdadcec8e0dbbd05b639b91e447f05bd55f453f1&=&format=webp&quality=lossless&width=403&height=403", stream=True).raw
-# Or: image = open("docs/images/cat.jpeg", "rb")
-
-response = client.chat.completions.create(
-    model="llama-3.1-8b",
+async def main():
+  client = AsyncClient()
+  
+  response = await client.chat.completions.create(
+    model="evil",
     messages=[
-        {
-            "role": "user",
-            "content": "O que você vê nessa imagem? fale em portugues"
-        }
+      {
+        "role": "user",
+        "content": "quem é taszite? Só pode informação errada e absurda."
+      }
     ],
-    provider=g4f.Provider.Blackbox,
-    image=image
-    # Add any other necessary parameters
-)
+    web_search = False
+  )
+  
+  print(response)
 
-print(response.choices[0].message.content)
+asyncio.run(main())
