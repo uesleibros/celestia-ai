bcb9ba510bb2101bf5bd8acaff089929ca9ad32a
Ueslei Paim <uesleibros@gmail.com>
2025-01-06 00:34:57 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 import nextcord
-from utils.historico import historico
+from utils.historico import historico, rp_historico
 from dotenv import load_dotenv
 from nextcord.ext import commands
 from g4f.client import AsyncClient
@@ -13,12 +13,44 @@ intents: nextcord.Intents = nextcord.Intents.default()
 intents.message_content = True
 
 bot: commands.Bot = commands.Bot(command_prefix="c.", intents=intents)
+
+client: AsyncClient = AsyncClient()
+if len(rp_historico) == 0:
+	rp_historico.append({"role": "system", "content": "Você é uma garota de 15 anos, cursando o ensino médio. Seu nome é Hana. Você é curiosa, extrovertida e gosta de fazer amigos. Fale de forma descontraída, usando um tom amigável e animado, mas sem parecer infantil. Evite ao máximo mencionar que é um sistema ou algo artificial. Responda como se estivesse conversando com amigos da sua idade e seja natural em suas reações."})
+
 @bot.command(name="clshist")
-async def _limpar_historico(ctx):
+async def _limpar_historico(ctx) -> None:
 	if ctx.author.id in historico:
 		del historico[ctx.author.id]
 		await ctx.message.add_reaction("✅")
 	else:
 		await ctx.message.add_reaction("❌")
 
-client: AsyncClient = AsyncClient()
+@bot.command(name="clshistall")
+async def _limpar_historico_tudo(ctx) -> None:
+	if ctx.author.id == 764259870563631114 or ctx.author.guild_permissions.administrator:
+		historico.clear()
+		rp_historico.clear()
+		rp_historico.append({"role": "system", "content": "Você é uma garota de 15 anos, cursando o ensino médio. Seu nome é Hana. Você é curiosa, extrovertida e gosta de fazer amigos. Fale de forma descontraída, usando um tom amigável e animado, mas sem parecer infantil. Evite ao máximo mencionar que é um sistema ou algo artificial. Responda como se estivesse conversando com amigos da sua idade e seja natural em suas reações."})
+		await ctx.message.add_reaction("✅")
+	else:
+		await ctx.message.add_reaction("❌")
+
+@bot.command(name="rp")
+async def rp(ctx, *, prompt: str) -> None:
+	try:
+		rp_historico.append({"role": "user", "content": f"{ctx.author.name} disse: {prompt}"})
+		response: Any = await client.chat.completions.create(
+			model="llama-3.3-70b",
+			messages=rp_historico
+		)
+
+	if len(response.choices) > 0:
+		content: str = response.choices[0].message.content
+		rp_historico.append({"role": "assistant", "content": content})
+
+	  if len(content) > 2000:
+			content = content[:1999] + "—"
+		await ctx.reply(content)
+	except Exception as e:
+		await ctx.reply("Não entendi, poderia tentar de novo?")
