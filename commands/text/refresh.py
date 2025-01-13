f28b2d5273780bbb4f47e15582637a5a8d152d62
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2025-01-13 17:54:58 -0300

    arrumado indentação.

--- commands/text/refresh.py
+++ commands/text/refresh.py
@@ -1,16 +1,6 @@
 from config import bot
-from utils.historico import system_context, rp_modelo
-from utils.rp.sys_context import create_system_context
+from utils.historico import rp_modelo
 from structures.text_models import TEXT_MODELS, DEFAULT_RP_MODEL
-
-@bot.command(name="refreshsvctx")
-async def _refrescar_contexto_servidor(ctx) -> None:
-	global system_context
-	if ctx.author.id == 764259870563631114 or ctx.author.guild_permissions.administrator:
-		system_context = await create_system_context(ctx.message.guild)
-		await ctx.message.add_reaction("✅")
-	else:
-		await ctx.message.add_reaction("❌")
 
 @bot.command(name="refreshmod")
 async def _refrescar_modelo(ctx) -> None:
